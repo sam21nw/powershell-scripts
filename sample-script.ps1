14fe9ea0c6af3dffb1b3fa8cea94a296147ef46e
Sam Jacob <sam21nw@gmail.com>
2022-05-29 07:29:31 +0400

    updated

--- sample-script.ps1
+++
@@ -1,6 +0,0 @@
-$Server = Read-Host -Prompt 'Input your server  name'
-$User = Read-Host -Prompt 'Input the user name'
-$Date = Get-Date
-Write-Host "You input server '$Server' and '$User' on '$Date'" 
-
-pause
